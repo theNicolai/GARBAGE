4ea9fda074cd5823e21b23f3b39bcd9f6497e401
theNicolai <35096019+theNicolai@users.noreply.github.com>
2018-01-09 11:26:00 +0100

    Add files via upload

---
+++ LDR_3V3/LDR_3V3.ino
@@ -0,0 +1,19 @@
+
+  
+  // put your setup code here, to run once:
+int sensorPin = A0; // select the input pin for LDR
+int sensorValue = 0; // variable to store the value coming from the sensor
+
+void setup() {
+Serial.begin(115200); //sets serial port for communication
+}
+
+
+
+
+
+void loop() {
+sensorValue = analogRead(sensorPin); // read the value from the sensor
+Serial.println(sensorValue); //prints the values coming from the sensor on the screen
+delay(100);
+}
