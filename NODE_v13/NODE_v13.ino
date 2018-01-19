6a90d7ccc5ade87efc5dca989ef106fade6fc254
theNicolai <35096019+theNicolai@users.noreply.github.com>
2018-01-19 14:39:40 +0100

    HAND IN FILES: UNO_v13 and NODE_v13

---
+++ NODE_v13/NODE_v13.ino
@@ -0,0 +1,266 @@
+/*
+  This script is compatible with UNO_v10_clean
+  This script recives a char via bluetooth, from the UNO.
+  This char is deciphered and sent to ThingSpeak, and
+  a local Website hosted on the NodeMCU esp8266.
+  Hardware:
+  -  NodeMCU esp8266
+  -  HM-10 BLE (conected to another HM-10 BLE)
+*/
+
+
+// FOR BLUETOOTH
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(D6, D7); // RX, TX read/write pins to BT-unit
+char blu = 'a'; // "raw-data" recived form BlueTooth. Used in Send() and ThinkValue() functions.
+
+/// FOR WEBSITE
+const char* ssid     = "DTU-Networks";  // Network name
+const char* password = "Fotonik343"; // Network password
+
+#include <ESP8266WiFi.h> // ESP8266WiFi.h library
+#include <ESP8266WebServer.h> //ESP8266WebServer.h library
+#include <WiFiClient.h> // WiFiClient.h library
+
+WiFiServer server(80);    // Creates a server that listens for incoming connections on port 80.
+String HTTP_req;          // Stores the HTTP request.
+
+char c = ' '; //the char sent from the Arduino UNO, to be used in function Send()
+String GarbageStatus = "GarbageStatus"; // Return String from Send() function. Contains bin-status for HTLM-website.
+
+
+//// FOR ThingSpeak
+int full = 0; // Sends an integer value to ThingSpeak, with the garbage amount in percent.
+const char* host = "api.thingspeak.com";      // API
+const char* writeAPIKey = "1Z76DOYEDDLQ132R"; // API-Key
+
+
+//// FOR Timetracking in loop
+unsigned long HtlmPrevMillis = 0;
+unsigned long ThingSpeakPrevMillis = 0;
+
+const long HtlmInterval = 8 * 1000; // 8 seconds
+const long ThingSpeakInterval = 30 * 1000;  // 30 seconds
+
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+////////// SETUP
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void setup() {
+  // Serial print and my serial setup
+  Serial.begin(9600);
+  Serial.println("Jeg starter");
+
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600); // Set Baud rate for Bluetooth
+  delay(100);
+
+  //  Connect to WiFi network
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+  Serial.println("Server started");
+  WiFiClient client = server.available();
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/////////////// LOOP START
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void loop() {
+
+  // Counts milliseconds from program start. Will overflow in approx 50 days.
+  // See https://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
+  unsigned long currentMillis = millis();
+
+  /// FOR BlueTooth
+  if (mySerial.available()) { // If BT-unit recives data
+    blu = mySerial.read(); // Asign incomming char to blu
+    delay(2);
+    Send(blu); // Call function Send()
+    Serial.println(GarbageStatus); // Show return from Send()
+  }
+
+  ///// HTLM WEBSITE
+  //If 8 seconds or more has passed, then re-run the Website (refresh the info on the page)
+  if (currentMillis - HtlmPrevMillis >= HtlmInterval) {
+    HTML(GarbageStatus);          //Call function HTLM(). Run website (update)
+    HtlmPrevMillis = currentMillis; // Reset timer
+  }
+
+  //If 30 seconds or more has passed, then send current trash-percentage to ThinkSpeak. (refresh the info on the page)
+  if (currentMillis - ThingSpeakPrevMillis >= ThingSpeakInterval) {
+    Serial.println("THINK THINK THINK!!!!!");
+    ThingSpeakFunc();     // Call function ThinSpeakFunc(). (Send data to ThingSpeak)
+    ThingSpeakPrevMillis = currentMillis; // Reset timer.
+  }
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// END OF LOOP
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// The function Send is used to "decrypt" the char sent from the Arduino UNO. (See function Message in NODE code).
+// The Arduino UNO sends either a ('a','b','c','d','e','f').
+// This is converted to a string, ready for insertion in the HTML function.
+
+String Send(char c) {
+  if (c == 'a') {
+    GarbageStatus = "10% full";
+  }
+
+  else if (c == 'b') {
+    GarbageStatus = "30% full";
+  }
+
+  else if (c == 'c') {
+    GarbageStatus = "50% full";
+  }
+
+  else if (c == 'd') {
+    GarbageStatus = "70% full";
+  }
+
+  else if (c == 'e') {
+    GarbageStatus = "100% full";
+  }
+
+  else if (c == 'f') {
+    GarbageStatus = "door open, unable to read";
+  }
+
+  else { //The char is discarded
+  }
+  return (GarbageStatus);
+}
+
+//////////////////
+
+//Similar to Send(), this function takes the same char ('a','b','c','d','e','f'),
+// but converts into a int, ready for the ThinkSpeakFunc().
+int ThinkValue(char c) {
+  if (c == 'a') {
+    full = 10;
+  }
+
+  else if (c == 'b') {
+    full = 30;
+  }
+
+  else if (c == 'c') {
+    full = 50;
+  }
+
+  else if (c == 'd') {
+    full = 70;
+  }
+
+  else if (c == 'e') {
+    full = 100;
+  }
+
+  else {
+  }
+  return (full);
+}
+
+///////////
+
+// This function sends the garbage full percentage to ThinkSpeak.
+// It uses the the ThinkValue() function to send an int instead of a char.
+// Code built uppon: http://www.instructables.com/id/Send-sensor-data-DHT11-BMP180-to-ThingSpeak-with-a/
+
+
+void ThingSpeakFunc() {
+  // ThingSpeak
+  delay(500);
+  // make TCP connections
+  const int httpPort = 80;
+  WiFiClient client = server.available();
+  if (!client.connect(host, httpPort)) {
+    return;
+  }
+  String url = "/update?key=";
+  url += writeAPIKey;
+  url += "&field1=";
+  url += String(ThinkValue(blu));
+  url += "\r\n";
+
+  // Request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+}
+
+
+//////////////////////////////
+
+// This function sends the converted sensordata to at website hosted on the NodeMCU espESP8266.
+// It uses the return value from the Send function.
+// Built upon this code: http://web-files.ait.dtu.dk/mnpe/WiFiWebServer_LED.txt
+
+void HTML(String GarbageStatus) {
+  // WEBSERVER HTML
+  WiFiClient client = server.available();
+
+  if (client) {  // got client?
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {   // client data available to read
+        char c = client.read(); // read 1 byte (character) from client
+        HTTP_req += c;  // save the HTTP request 1 char at a time
+        // last line of client request is blank and ends with \n
+        // respond to client only after last line received
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");
+          client.println("Refresh: 5"); // Refresh webpage
+          client.println();
+          // send web page
+          client.println("<!DOCTYPE html>");
+          client.println("<html>");
+          client.println("<head>");
+          client.println("<title>Arduino Garbage Control</title>");
+          client.println("</head>");
+          client.println("<body>");
+          client.println("<h1>Garbage Can Status:</h1>");
+
+          client.println("<b>Current Garbage Status: </b>");
+          client.println(GarbageStatus);
+          client.println("</form>");
+          client.println("<body>");
+          client.println("<html>");
+
+          Serial.print(HTTP_req);
+          HTTP_req = "";    // finished with request, empty string
+          break;
+        }
+        // every line of text received from the client ends with \r\n
+        if (c == '\n') {
+          // last character on line of received text
+          // starting new line with next character read
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          // a text character was received from client
+          currentLineIsBlank = false;
+        }
+      } // end if (client.available())
+    } // end while (client.connected())
+    delay(1);      // give the web browser time to receive the data
+    client.stop(); // close the connection
+  } // end if (client)
+}
---
+++ UNO_v13/UNO_v13.ino
@@ -0,0 +1,181 @@
+/*
+  This script is compatible with NODE_v10_clean
+  This script sends a char via. bluetooth.
+  The char change based upon values from a LDR and an ultrasonic distance measurer.
+  Hardware:
+  -  Arduino UNO
+  -  HM-10 BLE (conected to another HM-10 BLE)
+  -  LDR
+  -  Grove Ultrasonic Ranger
+  -  Resistors 2x 2kΩ and 1x 1kΩ
+*/
+
+
+#include <AltSoftSerial.h>
+AltSoftSerial BTserial;
+//
+// https://www.pjrc.com/teensy/td_libs_AltSoftSerial.html
+/*
+  SerialIn_SerialOut_HM-10_01
+  Uses hardware serial to talk to the host computer and AltSoftSerial for communication with the bluetooth module
+  Whatever is entered in the serial monitor is sent to the connected device.
+  Anything received from the connected device is copied to the serial monitor.
+  This is used to hard code both HM-10 modueles.
+  Does not send line endings to the HM-10
+  Pins
+  BT VCC to Arduino 5V out.
+  BT GND to GND
+  Arduino D8 (SS RX) - BT TX no need voltage divider
+  Arduino D9 (SS TX) - BT RX through a voltage divider (5v to 3.3v)
+*/
+
+
+// FOR HTML Website
+char c = ' ';
+boolean NL = true;
+
+// FOR light dependant resistor
+int LDRPin = A0; // Pin used to analog.read the LDR value.
+boolean DoorOpen = true; // initialise cupboard door, where kitchen rubbish bin is placed, to be open
+
+// FOR DISTANCE
+int pingPin = 6; // Pin that sends and recives ultrasonic waves
+float binTop = 5; // Distance from USM to top of rubbish bin in cm
+float binBottom = 100; // Distance from USM to bottom of rubbish bin in cm
+float full = 0; // full tells how full the bin is in percent.
+int roundup = 0; // initialising the variable, which adjust full to specific intervals
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//////////      SETUP
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void setup()
+{
+  Serial.begin(9600);
+
+  // Setup for Bluetooth unit
+  BTserial.begin(9600);
+  BTserial.print("AT+IMME1" ); //Turn off auto connect
+  delay(1000);
+  BTserial.print("AT+ROLE1" );  // Make HM-10 Master
+  delay(1000);
+  BTserial.print("AT+CON508CB166140E"); //Connect to known MAC addres of other BT-slave
+  delay(1000);
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+///////////    LOOP START
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void loop()
+{
+  DoorStatus(); // Check if door is open. Return boolean DoorOpen
+  if (DoorOpen == 1) { // If door is closed
+    Distance(); // Measure distance, return full.
+    Message(full); // convert full to char. Send char to bluetooth and serial monitor.
+  }
+
+  // If door is open: dont send measurement. instead send "f"
+  // "f" from the NodeMCU understood as "door is open! can't read"
+  else {
+    Serial.println("f"); // Send "f" to Bluetooth
+    BTserial.print("f"); //
+  }
+  delay(1000); // Slows the send rate down, to allow the NodeMCU to keep up.
+
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+////////    LOOP END
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+// Taken from https://www.arduino.cc/en/Tutorial/Ping
+long microsecondsToCentimeters(long microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the object we
+  // take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
+
+///////////////////////////////////////////////////////////
+// This function activates the Ultrasonic distance measurer, and converts the data into centimeters.
+// Based on top and bottom of the bin, it then calculates the percentwise fullness of the bin.
+// Taken from https://www.arduino.cc/en/Tutorial/Ping
+float Distance () {
+  // establish variables for duration of the ping, and the distance result in cencimeters:
+  float duration, cm;
+
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pingPin, LOW);
+
+  // The same pin is used to read the signal from the PING))): a HIGH pulse
+  // whose duration is the time (in microseconds) from the sending of the ping
+  // to the reception of its echo off of an object.
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+
+  // convert the time into a distance
+  cm = microsecondsToCentimeters(duration);
+  delay(2);
+  // Calculate bin full percentwise
+  full = float(100 - (cm / (binBottom - binTop)) * 100); // Perscenwise full %
+
+  return full;
+}
+
+
+////////////////////////////
+//  Takes full, converts to the integer roundup.
+//  if roundup fits an interval, the coresponding char wil be sent via Bluetooth.
+char Message(float full) {
+  roundup = (int) full; 
+  if ( roundup < 20) {
+    Serial.println("a"); // EMPTY
+    BTserial.print("a");
+  }
+
+  else if (20 < roundup && roundup < 40) {
+
+    Serial.println("b");  // 30%
+    BTserial.print("b");
+  }
+
+  else if (40 < roundup && roundup < 60) {
+    Serial.println("c");  // 50%
+    BTserial.print("c");
+  }
+
+  else if ( 60 < roundup && roundup < 85) {
+    Serial.println("d");  // 70%
+    BTserial.print("d");
+  }
+
+  else if (roundup > 85) {
+    Serial.println("e");  // FULL
+    BTserial.print("e");
+  }
+}
+
+///////////////////////////////////////////////////
+// DoorStatus checks if the cupboard is open.
+// This is done to avoid false readings on the egde of the bin, or bellow it if removed for emptying.
+// This is done by reading the LDR value.
+boolean DoorStatus() {
+  int sensorRead = analogRead(LDRPin);
+  if (sensorRead < 700) { // if too bright then door is open
+    DoorOpen = false;
+  }
+  else {          // Door closed
+    DoorOpen = true;
+  }
+  return DoorOpen; // Return a boolean used to deside wrether to read from ultrasonic ranger or not.
+}
